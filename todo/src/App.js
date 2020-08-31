0832b6f07192778731ab20b038e433462ec92b9c
Selfmade20 <tebogo1686@gmail.com>
2020-08-31 13:11:38 +0200

    delete todo

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,37 +1,54 @@
-import React, { useState } from 'react';
-import './App.css';
-import TodoForm from './components/todoForm'
+import React, { useState } from "react";
+import "./App.css";
+
+
+export default function TodoForm() {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  const addTodo = () => {};
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter your todo here....."
+        className="input"
+        value={value}
+      />
+    </form>
+  );
+}
 
 function Todo({ todo, index }) {
-  return (
-    <div className="todo">
-      {todo.text}
-    </div>
-  )
+  return <div className="todo">{todo.text}</div>;
 }
 
 
 
 function App() {
-
   const [todos, setTodos] = useState([
     {
       text: "Clean the house and make up the bed",
-      isCompleted: false
+      isCompleted: false,
     },
     {
       text: "Create a todo app",
-      isCompleted: false
+      isCompleted: false,
     },
     {
       text: "Excecise and do home workouts",
-      isCompleted: false
+      isCompleted: false,
     },
-  ])
+  ]);
 
-  const addTodo = () => {
-
-  }
+  const addTodo = () => {};
   return (
     <div className="app">
       <div className="todo-list">
